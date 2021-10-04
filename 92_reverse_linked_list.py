8ea2da759a4d131065484b05be33ab990f76430b
Moongee Cho <35756853+mcdaniel13@users.noreply.github.com>
2021-10-04 01:10:47 -0500

    Update 92_reverse_linked_list.py

--- 92_reverse_linked_list.py
+++ 92_reverse_linked_list.py
@@ -1,6 +1,3 @@
-from typing import List
-from typing import Optional
-
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -8,41 +5,39 @@ class ListNode:
 
 
 class Solution:
-    def create(self, nums: List[int]) -> ListNode:
-        head = ListNode()
-        cur = head
-        for i in range(len(nums)):
-            cur.val = nums[i]
-            if i != len(nums) - 1:
-                cur.next = ListNode()
-                cur = cur.next
-
-        return head
-
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        if not head.next and left == right:
-            return head
-
-        arr = [None]
         cur = head
+        prev = None
+        cnt = 1
+        while cur:
+            if cnt == left:
+                start = cur
+                break
+            prev = cur
+            cur = cur.next
+            cnt += 1
+
+        arr = []
         while cur:
             arr.append(cur)
+            if cnt == right:
+                end = cur
+                break
             cur = cur.next
-        arr.append(None)
+            cnt += 1
 
-        for i in range(right, left - , -1):
+        next = cur.next
+        for i in range(len(arr) - 1, 0, -1):
             arr[i].next = arr[i - 1]
 
-        if arr[left - 1]:
-            arr[left - 1].next = arr[right]
-
-        if arr[right + 1]:
-            arr[left].next = arr[right + 1]
+        if prev:
+            prev.next = arr[-1]
         else:
-            arr[left].next = None
+            head = arr[-1]
 
-        return arr[1]
+        if next:
+            arr[0].next = next
+        else:
+            arr[0].next = None
 
-sol = Solution()
-l = sol.create([3, 5])
-sol.reverseBetween(l, 1, 2)
+        return head
